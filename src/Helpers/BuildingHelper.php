7dbeb870e750a4b94400f6c651a5d7fb1836d74b
Dr. Eki <dreki@lime.st>
2019-08-18 17:32:48 +0300

    Update BuildingHelper.php

--- src/Helpers/BuildingHelper.php
+++ src/Helpers/BuildingHelper.php
@@ -9,34 +9,27 @@ class BuildingHelper
 
     public function getBuildingTypes(): array
     {
-        if($dominion->race->name == 'Void')
-        {
-            return [];
-        }
-        else
-        {
-            return [
-                'home',
-                'alchemy',
-                'farm',
-                'smithy',
-                'masonry',
-                'ore_mine',
-                'gryphon_nest',
-                'tower',
-                'wizard_guild',
-                'temple',
-                'diamond_mine',
-                'school',
-                'lumberyard',
-                'forest_haven',
-                'factory',
-                'guard_tower',
-                'shrine',
-                'barracks',
-                'dock',
-            ];
-        }
+        return [
+            'home',
+            'alchemy',
+            'farm',
+            'smithy',
+            'masonry',
+            'ore_mine',
+            'gryphon_nest',
+            'tower',
+            'wizard_guild',
+            'temple',
+            'diamond_mine',
+            'school',
+            'lumberyard',
+            'forest_haven',
+            'factory',
+            'guard_tower',
+            'shrine',
+            'barracks',
+            'dock',
+        ];
     }
 
     public function getBuildingTypesByRace(Race $race = null): array
