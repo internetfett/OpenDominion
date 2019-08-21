f7782e875ad67d1f2c99f911b25ed1c83c036f6f
Dr. Eki <dreki@lime.st>
2019-08-21 14:51:06 +0300

    Boats

--- src/Services/Dominion/API/InvadeCalculationService.php
+++ src/Services/Dominion/API/InvadeCalculationService.php
@@ -18,10 +18,10 @@ class InvadeCalculationService
     /**
      * @var int How many units can fit in a single boat
      */
-    $units_per_boat = 30;
-    $units_per_boat += $dominion->race->getPerkValue('boat_capacity');
+#    $units_per_boat = 30;
+#    $units_per_boat += $dominion->race->getPerkValue('boat_capacity');
 
-    protected const UNITS_PER_BOAT = $units_per_boat;
+    protected const UNITS_PER_BOAT = 20 + $dominion->race->getPerkValue('boat_capacity');
 
     /** @var BuildingCalculator */
     //protected $buildingCalculator;
--- src/Services/Dominion/Actions/InvadeActionService.php
+++ src/Services/Dominion/Actions/InvadeActionService.php
@@ -81,7 +81,7 @@ class InvadeActionService
 #    $units_per_boat = 30;
 #    $units_per_boat += $dominion->race->getPerkValue('boat_capacity');
 
-    protected const UNITS_PER_BOAT = 30 + $dominion->race->getPerkValue('boat_capacity');
+    protected const UNITS_PER_BOAT = 20 + $dominion->race->getPerkValue('boat_capacity');
 
 
     /** @var BuildingCalculator */
