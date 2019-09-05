879c74c4007dad81609a82e65cfce23e8f7145ff
Dr. Eki <dreki@lime.st>
2019-09-05 11:50:53 +0300

    Starve fix Void/Spirit

--- app/resources/views/pages/dominion/explore.blade.php
+++ app/resources/views/pages/dominion/explore.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-@if ($selectedDominion->resource_food > 0)
+@if ($selectedDominion->resource_food > 0 or $selectedDominion->race->getPerkMultiplier('food_production') = -100)
 
     <div class="row">
 
