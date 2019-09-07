9f271b0735a18021d2d32e6e8449e231fdd92acb
Dr. Eki <dreki@lime.st>
2019-09-07 18:57:24 +0300

    Update invade.blade.php

--- app/resources/views/pages/dominion/invade.blade.php
+++ app/resources/views/pages/dominion/invade.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-@if ($selectedDominion->race == 'Growth' and !(bool)$selectedDominion->spellCalculator->isSpellActive($selectedDominion, 'woven_nerves'))
+@if ($selectedDominion->race == 'Growth')
 <div class="row">
     <div class="col-sm-12 col-md-9">
         <div class="box box-primary">
