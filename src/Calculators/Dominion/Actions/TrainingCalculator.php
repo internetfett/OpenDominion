ad1df3b25ac8d8db89bc9bf0740cb23f63bb673c
Dr. Eki <dreki@lime.st>
2019-09-09 16:27:20 +0300

    Update TrainingCalculator.php

--- src/Calculators/Dominion/Actions/TrainingCalculator.php
+++ src/Calculators/Dominion/Actions/TrainingCalculator.php
@@ -167,7 +167,9 @@ class TrainingCalculator
             foreach ($costs as $type => $value) {
               if($value == 0 or $value == Null)
               {
-                #$value = 1;
+                echo "THIS IS THE VALUE:" $value;
+                var_dump($value);
+                $value = 1;
               }
                 $trainableByCost[$type] = (int)floor($dominion->{$fieldMapping[$type]} / $value);
             }
