ad0bde8c2e4e25e7759b463454fa0f256c202ec9
Dr. Eki <dreki@lime.st>
2019-09-12 15:46:14 +0300

    Update TrainActionService.php

--- src/Services/Dominion/Actions/Military/TrainActionService.php
+++ src/Services/Dominion/Actions/Military/TrainActionService.php
@@ -111,13 +111,6 @@ class TrainActionService
         }
 
         // If training elites, check if ARMADA or IMPERIAL GNOME to calculate unit housing (Docks / Factories)
-        if(
-          ($unitsToTrain['military_unit3'] > 0) or
-          ($unitsToTrain['military_unit4'] > 0) or
-          ($unitsToTrain['unit3'] > 0) or
-          ($unitsToTrain['unit4'] > 0)
-          )
-        {
           // ARMADA: Max 2 Boats per Dock (+ Harbour)
           if ($dominion->race->name == 'Armada' and (($dominions->military_unit3 + $dominions->military_unit4) + ($unitsToTrain['unit3'] + $unitsToTrain['unit4'])) > ($dominions->building->docks * /*(HARBOR * 1 ) * */ 2))
           {
@@ -128,7 +121,7 @@ class TrainActionService
           {
             throw new GameException('You cannot control that many machines. Max 2 machines per Factory. Increased by Science.');
           }
-        }
+        
 
         if ($totalCosts['draftees'] > $dominion->military_draftees) {
             throw new GameException('Training aborted due to lack of draftees');
