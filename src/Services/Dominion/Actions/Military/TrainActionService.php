425d4dea6c171b34598828e8ccdcead002238edd
Dr. Eki <dreki@lime.st>
2019-09-12 15:49:51 +0300

    Update TrainActionService.php

--- src/Services/Dominion/Actions/Military/TrainActionService.php
+++ src/Services/Dominion/Actions/Military/TrainActionService.php
@@ -112,7 +112,7 @@ class TrainActionService
 
           // If training elites, check if ARMADA or IMPERIAL GNOME to calculate unit housing (Docks / Factories)
           // ARMADA: Max 2 Boats per Dock (+ Harbour)
-          if ($dominion->race->name == 'Armada' and (($dominion->military_unit3 + $dominion->military_unit4) + ($unitsToTrain[3] + $unitsToTrain[4])) > ($dominion->building->docks * /*(HARBOR * 1 ) * */ 2))
+          if ($dominion->race->name == 'Armada' and (($dominion->military_unit3 + $dominion->military_unit4) + ($unitsToTrain['unit3'] + $unitsToTrain['unit4'])) > ($dominion->building->docks * /*(HARBOR * 1 ) * */ 2))
           {
             throw new GameException('You cannot control that many ships. Max 2 ships per Dock. Increased by Harbor.');
           }
