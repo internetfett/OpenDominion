56d9d74aca93813cf0858b8a884e1db7b38affb6
Dr. Eki <dreki@lime.st>
2019-09-14 21:13:33 +0300

    Update invasion.blade.php

--- app/resources/views/pages/dominion/event/invasion.blade.php
+++ app/resources/views/pages/dominion/event/invasion.blade.php
@@ -244,7 +244,7 @@
 
                             @if (isset($event->data['attacker']['champion']) && $event->source->id === $selectedDominion->id)
                                 <p class="text-center text-green">
-                                  {{ $unitHelper->getSoulCollectionString($event->data['attacker']['champion']['champion']) }}
+                                  {{ $unitHelper->getSoulCollectionString($event->data['attacker']['champion']['champions']) }}
                                 </p>
                             @endif
 
