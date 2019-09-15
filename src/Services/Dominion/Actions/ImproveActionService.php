cabc4578d3cc0a5983963a3f1d6a641273183974
Dr. Eki <dreki@lime.st>
2019-09-15 21:11:23 +0300

    Update ImproveActionService.php

--- src/Services/Dominion/Actions/ImproveActionService.php
+++ src/Services/Dominion/Actions/ImproveActionService.php
@@ -37,7 +37,7 @@ class ImproveActionService
             throw new GameException('Investment aborted due to bad input.');
         }
 
-        if (!\in_array($resource, ['platinum', 'lumber', 'ore', 'gems','mana'], true)) {
+        if (!\in_array($resource, ['platinum', 'lumber', 'ore', 'gems','mana','food'], true)) {
             throw new GameException('Investment aborted due to bad resource type.');
         }
 
