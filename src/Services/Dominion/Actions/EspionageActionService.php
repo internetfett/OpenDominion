ba5b2cb3a8aca8c32e3ef46f79432b1450e02508
Dr. Eki <dreki@lime.st>
2019-09-21 14:33:03 +0300

    Update EspionageActionService.php

--- src/Services/Dominion/Actions/EspionageActionService.php
+++ src/Services/Dominion/Actions/EspionageActionService.php
@@ -659,7 +659,7 @@ class EspionageActionService
             elseif ($resource == 'ore')
                 $maxDominion = floor($this->productionCalculator->getOreProductionRaw($dominion) * $constraints['self_production'] / 100);
             elseif ($resource == 'gems')
-                $maxDominion = floor($this->productionCalculator->getGemsProductionRaw($dominion) * $constraints['self_production'] / 100);
+                $maxDominion = floor($this->productionCalculator->getGemProductionRaw($dominion) * $constraints['self_production'] / 100);
         }
 
         // Limit to amount carryable by spies
