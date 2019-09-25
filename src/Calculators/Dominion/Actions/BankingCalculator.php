1d853109045eb89d5c33501495132924f8712c30
Dr. Eki <dreki@lime.st>
2019-09-25 18:49:35 +0300

    Demon

--- src/Calculators/Dominion/Actions/BankingCalculator.php
+++ src/Calculators/Dominion/Actions/BankingCalculator.php
@@ -47,14 +47,26 @@ class BankingCalculator
             ],
         ];
 
+        if($dominion->race->name == 'Demon')
+        {
+          $resources[] = [
+            'resource_soul' => [
+                'label' => 'Souls',
+                'buy' => 3.0,
+                'sell' => 0.0,
+                'max' => $dominion->resource_gems,
+            ],
+            'resource_peasant' => [
+                'label' => 'Peasants',
+                'buy' => 0.0,
+                'sell' => 3.0,
+                'max' => $dominion->resource_food,
+            ],
+          ];
+        }
+
         // Get racial bonus
         $bonus = $dominion->race->getPerkMultiplier('exchange_bonus');
-#        $resources['resource_platinum']['buy'] *= (1 + $bonus);
-#        $resources['resource_lumber']['buy'] *= (1 + $bonus);
-#        $resources['resource_ore']['buy'] *= (1 + $bonus);
-#        $resources['resource_gems']['buy'] *= (1 + $bonus);
-#        $resources['resource_food']['buy'] *= (1 + $bonus);
-
 
         $resources['resource_platinum']['sell'] *= (1 + $bonus);
         $resources['resource_lumber']['sell'] *= (1 + $bonus);
