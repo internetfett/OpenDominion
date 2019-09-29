b60e46b2bd206c5a40793bd3ef8262efdf1a58d1
Dr. Eki <dreki@lime.st>
2019-09-29 21:17:39 +0300

    Update RoundController.php

--- src/Http/Controllers/RoundController.php
+++ src/Http/Controllers/RoundController.php
@@ -167,8 +167,7 @@ class RoundController extends AbstractController
 
         } catch (QueryException $e) {
 
-            $request->command->error("SQL Error: " . $e->getMessage() . "\n");
-            die();
+            die($e->getMessage());
             return redirect()->back()
                 ->withInput($request->all())
                 ->withErrors(["Someone already registered a dominion with the name '{$dominionName}' for this round."]);
