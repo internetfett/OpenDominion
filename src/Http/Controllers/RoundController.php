8a3c233dea0cea78ff90445f40468e723cacfb15
Dr. Eki <dreki@lime.st>
2019-09-29 21:09:01 +0300

    Update RoundController.php

--- src/Http/Controllers/RoundController.php
+++ src/Http/Controllers/RoundController.php
@@ -166,6 +166,8 @@ class RoundController extends AbstractController
             });
 
         } catch (QueryException $e) {
+
+            die('<pre>' . var_dump($e) . '</pre>');
             return redirect()->back()
                 ->withInput($request->all())
                 ->withErrors(["Someone already registered a dominion with the name '{$dominionName}' for this round."]);
