39b719d990226659c2b5a3b76c2d416e1cd232de
Dr. Eki <dreki@lime.st>
2019-10-03 15:41:13 +0300

    TC

---
+++ app/resources/views/pages/termsandconditions.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('page-header', 'Terms and Conditions')
+
+@section('content')
+    <div class="box">
+        <div class="box-body">
+            Terms and Conditions
+        </div>
+    </div>
+
+    <div class="row">
+        T&Cs go here.
+    </div>
+@endsection
