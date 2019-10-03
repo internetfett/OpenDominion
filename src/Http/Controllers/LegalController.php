1088798e683af7ebb91e18f6a0748d9048880ac2
Dr. Eki <dreki@lime.st>
2019-10-03 20:43:40 +0300

    Update LegalController.php

--- src/Http/Controllers/LegalController.php
+++ src/Http/Controllers/LegalController.php
@@ -5,28 +5,21 @@ namespace OpenDominion\Http\Controllers;
 class LegalController extends AbstractController
 {
 
-  $company_name = 'OD Arena';
-  $website_url = 'https://odarena.com/'
-  $website_name = 'OD Arena';
-  $company_address = 'Cyprus';
-  $company_jurisdiction = 'the Republic of Cyprus';
-  $contact_email = 'info@odarena.com';
-
     public function getIndex()
     {
         return view('pages.legal.index', [
-            'company_name' => $company_name,
-            'company_address' => $company_address,
+            'company_name' => 'OD Arena',
+            'company_address' => 'Cyprus',
         ]);
     }
 
     public function getTermsAndConditions()
     {
       return view('pages.legal.termsandconditions', [
-        'company_name' => $company_name,
-        'website_name' => $website_name,
-        'website_url' => $website_url,
-        'contact_email' => $contact_email,
+        'company_name' => 'OD Arena',
+        'website_name' => 'OD Arena',
+        'website_url' => 'https://odarena.com/',
+        'contact_email' => 'info@odarena.com',
       ]);
     }
 
@@ -34,9 +27,10 @@ class LegalController extends AbstractController
     public function getPrivacyPolicy()
     {
       return view('pages.legal.privacypolicy', [
-          'company_name' => $company_name,
-          'website_name' => $website_name,
-          'website_url' => $website_url,
+          'company_name' => 'OD Arena',
+          'website_name' => 'OD Arena',
+          'website_url' => 'https://odarena.com/',
+          'contact_email' => 'info@odarena.com',
       ]);
     }
 }
