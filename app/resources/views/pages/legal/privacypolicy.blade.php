c78e3cd1655d7a31c457da2009e0f5ab3e8cebcc
Dr. Eki <dreki@lime.st>
2019-10-03 16:19:58 +0300

    Update privacypolicy.blade.php

--- app/resources/views/pages/legal/privacypolicy.blade.php
+++ app/resources/views/pages/legal/privacypolicy.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="box-body">
 
-      At {{ ${{ $company_name }} }}, accessible from {{ $company_url }}, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by {{ ${{ $company_name }} }} and how we use it.
+      At {{ $company_name }}, accessible from {{ $company_url }}, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by {{ $company_name }} and how we use it.
 
       If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us through email at bikenopab@my6mail.com
       General Data Protection Regulation (GDPR)
@@ -38,30 +38,30 @@
 
       Log Files
 
-      {{ ${{ $company_name }} }} follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+      {{ $company_name }} follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
       Cookies and Web Beacons
 
-      Like any other website, {{ ${{ $company_name }} }} uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
+      Like any other website, {{ $company_name }} uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
       Privacy Policies
 
-      You may consult this list to find the Privacy Policy for each of the advertising partners of {{ ${{ $company_name }} }}.
+      You may consult this list to find the Privacy Policy for each of the advertising partners of {{ $company_name }}.
 
-      Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on {{ ${{ $company_name }} }}, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
+      Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on {{ $company_name }}, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.
 
-      Note that {{ ${{ $company_name }} }} has no access to or control over these cookies that are used by third-party advertisers.
+      Note that {{ $company_name }} has no access to or control over these cookies that are used by third-party advertisers.
       Third Party Privacy Policies
 
-      {{ ${{ $company_name }} }}'s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.
+      {{ $company_name }}'s Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.
 
       You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites. What Are Cookies?
       Children's Information
 
       Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
 
-      {{ ${{ $company_name }} }} does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
+      {{ $company_name }} does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
       Online Privacy Policy Only
 
-      Our Privacy Policy created at GDPRPrivacyPolicy.net) applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in {{ ${{ $company_name }} }}. This policy is not applicable to any information collected offline or via channels other than this website. Our GDPR Privacy Policy was generated from the GDPR Privacy Policy Generator.
+      Our Privacy Policy created at GDPRPrivacyPolicy.net) applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in {{ $company_name }}. This policy is not applicable to any information collected offline or via channels other than this website. Our GDPR Privacy Policy was generated from the GDPR Privacy Policy Generator.
       Consent
 
       By using our website, you hereby consent to our Privacy Policy and agree to its terms.
