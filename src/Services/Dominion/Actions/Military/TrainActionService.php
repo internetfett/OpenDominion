3425f97fa803c44ecd4af318b8716288aac19376
Dr. Eki <dreki@lime.st>
2019-10-09 21:29:05 +0300

    Update TrainActionService.php

--- src/Services/Dominion/Actions/Military/TrainActionService.php
+++ src/Services/Dominion/Actions/Military/TrainActionService.php
@@ -233,16 +233,16 @@ class TrainActionService
             {
               $pairingLimitedByTrained = $dominion->military_unit4;
             }
-            else
-            {
-              die(var_dump($pairingLimit));
-            }
 
-            if(($amountToTrain * $pairingLimit) < $pairingLimitedByTrained)
+            if(($amountToTrain * $pairingLimit) > $pairingLimitedByTrained)
             {
               throw new GameException('You cannot train that many units.');
             }
 
+          }
+          else
+          {
+            die($pairingLimit);
           }
 
         }
