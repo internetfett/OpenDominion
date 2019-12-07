9ebfc95f11e7fb6d2b5f4e0a84becf7cbc62b826
Dr. Eki <dreki@lime.st>
2019-12-07 14:37:48 +0200

    Update register.blade.php

--- app/resources/views/pages/round/register.blade.php
+++ app/resources/views/pages/round/register.blade.php
@@ -167,7 +167,7 @@
                     <div class="col-sm-offset-3 col-sm-9">
 
                       @if($round->hasStarted())
-                      <p>This round has already started and ends {{ $round->end_date->format('l, jS \o\f F Y \a\t G:i') }}.</p>
+                      <p>This round has already started and ends {{ $round->end_date->format('l, jS \o\f F Y \a\t H:i') }}.</p>
                       <p>To help you get going, you will get 0.5% extra starting resources for every hour since the round started.</p>
 
                         @if ($discordInviteLink = config('app.discord_invite_link'))
@@ -175,13 +175,13 @@
                         @endif
 
                       @else
-                      <p>The round starts on {{ $round->start_date->format('l, jS \o\f F Y \a\t G:i') }}.</p>
+                      <p>The round starts on {{ $round->start_date->format('l, jS \o\f F Y \a\t H:i') }}.</p>
                       <p>If 00:00 to 07:00 UTC are not convenient hours for you, consider registering a little later.</p>
 
                         @if ($discordInviteLink = config('app.discord_invite_link'))
                         <p>In the meantime, come join us on <a href="{{ $discordInviteLink }}" target="_blank">Discord</a>.</p>
                         @endif
-                        
+
                       @endif
                       </p>
                     </div>
