d4f134e14cb81135fcb88e84984c161fae212803
Dr. Eki <dreki@lime.st>
2019-12-07 09:25:46 +0200

    Update invade.blade.php

--- app/resources/views/pages/dominion/invade.blade.php
+++ app/resources/views/pages/dominion/invade.blade.php
@@ -35,7 +35,6 @@
                         <div class="box-header with-border">
                             <h3 class="box-title"><i class="ra ra-crossed-swords"></i> Invade</h3>
                         </div>
-                        <em style="color:#888;">Note that minimum raw DP is 10 DP per acre.</em>
                         <div class="box-body">
                             <div class="form-group">
                                 <label for="target_dominion">Select a target</label>
@@ -290,6 +289,10 @@
                                     </button>
                                   @endif
                                 </div>
+
+
+                                <p><em style="color:#888; font-size:80%;">Note that minimum raw DP is 10 DP per acre.</em></p>
+
                             </div>
 
                         </div>
