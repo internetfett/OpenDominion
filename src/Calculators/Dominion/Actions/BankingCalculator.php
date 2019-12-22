975f2f921e32771accfd65294167150831ceb307
Dr. Eki <dreki@lime.st>
2019-12-22 09:56:45 +0200

    Update BankingCalculator.php

--- src/Calculators/Dominion/Actions/BankingCalculator.php
+++ src/Calculators/Dominion/Actions/BankingCalculator.php
@@ -76,7 +76,7 @@ class BankingCalculator
           // Get racial bonus
           $bonus = $dominion->race->getPerkMultiplier('exchange_bonus');
           // Techs
-          $bonus += $dominion->getTechPerkMultiplier('construction_cost');
+          $bonus += $dominion->getTechPerkMultiplier('exchange_rate');
 
           $resources['resource_platinum']['sell'] *= (1 + $bonus);
           $resources['resource_lumber']['sell'] *= (1 + $bonus);
