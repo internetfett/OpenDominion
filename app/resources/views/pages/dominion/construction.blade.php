3fc45474e11a17f2ba3e6528e24c4ee05699ca3b
Dr. Eki <dreki@lime.st>
2020-01-03 16:03:57 +0200

    Update construction.blade.php

--- app/resources/views/pages/dominion/construction.blade.php
+++ app/resources/views/pages/dominion/construction.blade.php
@@ -77,12 +77,7 @@
                     @endif
 
                     <p>You have {{ number_format($landCalculator->getTotalBarrenLand($selectedDominion)) }} {{ str_plural('acre', $landCalculator->getTotalBarrenLand($selectedDominion)) }} of barren land
-                      @if($constructionCalculator->getMaxAfford($selectedDominion) > 0)
-                       and can afford to construct <strong>{{ number_format($constructionCalculator->getMaxAfford($selectedDominion)) }} {{ str_plural('building', $constructionCalculator->getMaxAfford($selectedDominion)) }}.</strong>
-                      @else
-                      .
-                      @endif
-                    </p>
+                      and can afford to construct <strong>{{ number_format($constructionCalculator->getMaxAfford($selectedDominion)) }} {{ str_plural('building', $constructionCalculator->getMaxAfford($selectedDominion)) }}</strong>.</p>
 
                     <p>You may also <a href="{{ route('dominion.destroy') }}">destroy buildings</a> if you wish.</p>
                 </div>
