89929b144ccf84f2baed654354142bbb274ce45c
Dr. Eki <dreki@lime.st>
2020-01-03 15:34:16 +0200

    Update construction.blade.php

--- app/resources/views/pages/dominion/construction.blade.php
+++ app/resources/views/pages/dominion/construction.blade.php
@@ -69,7 +69,7 @@
 
                     @if ($constructionCalculator->getCostMultiplier($selectedDominion) > 1)
 
-                    <p>Construction bonuses are lowering your costs by <strong>{{ number_format($constructionCalculator->getCostMultiplier($selectedDominion)-1)*100, 2) }}%</strong>.</p>
+                    <p>Construction bonuses are lowering your costs by <strong>{{ number_format(($constructionCalculator->getCostMultiplier($selectedDominion)-1)*100, 2) }}%</strong>.</p>
                     @endif
 
                     @if ($selectedDominion->discounted_land)
