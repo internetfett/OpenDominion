694ddd7b1b11bf1f02a50ef5b336fdc485a1e37d
Dr. Eki <dreki@lime.st>
2020-01-04 06:42:00 +0200

    Update rezone.blade.php

--- app/resources/views/pages/dominion/rezone.blade.php
+++ app/resources/views/pages/dominion/rezone.blade.php
@@ -85,7 +85,7 @@
                     </p>
 
                     @if ($rezoningCalculator->getCostMultiplier($selectedDominion) !== 1)
-                    <p>Bonuses are modifying your rezoning costs by <strong>{{ number_format((1-$constructionCalculator->getCostMultiplier($selectedDominion))*100, 2) }}%</strong>.</p>
+                    <p>Bonuses are modifying your rezoning costs by <strong>{{ number_format((1-$rezoningCalculator->getCostMultiplier($selectedDominion))*100, 2) }}%</strong>.</p>
                     @endif
 
                     <p>You have {{ number_format($landCalculator->getTotalBarrenLand($selectedDominion)) }} {{ str_plural('acre', $landCalculator->getTotalBarrenLand($selectedDominion)) }} of barren land
