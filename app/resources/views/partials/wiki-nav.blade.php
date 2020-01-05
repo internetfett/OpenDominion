046ac9ef31ccb3cae3255fc8e654528773ffc118
Dr. Eki <dreki@lime.st>
2020-01-05 14:32:29 +0200

    Update wiki-nav.blade.php

--- app/resources/views/partials/wiki-nav.blade.php
+++ app/resources/views/partials/wiki-nav.blade.php
@@ -1,3 +1,3 @@
 <li>
-    <a href="https://odarena.miraheze.org/{{ Route::getCurrentRoute() }}" target="_blank">Wiki</a>
+    <a href="https://odarena.miraheze.org/{{ Route::currentRouteName() }}" target="_blank">Wiki</a>
 </li>
