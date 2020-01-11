c418c4fb3a5c08ce9805065d63c966940503fbdb
Dr. Eki <dreki@lime.st>
2020-01-11 10:28:10 +0200

    Update termsandconditions.blade.php

--- app/resources/views/pages/legal/termsandconditions.blade.php
+++ app/resources/views/pages/legal/termsandconditions.blade.php
@@ -38,15 +38,15 @@ ol.tc li:before
 
       <p>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. The Terms and Conditions agreement  for {{ $website_name }} has been created with the help of <a href="https://www.termsfeed.com/">TermsFeed</a>.</p>
 
-      <ol>
+      <ol class="tc">
         <li>Source Code
-          <ol>
+          <ol class="tc">
             <li>The ODArena source code is freely available and subject to GNU Affero General Public License v3.0, which can be found at the following URL: https://www.gnu.org/licenses/agpl-3.0.en.html</li>
             <li>Nothing in these Terms and Conditions shall be construed to impose any restriction on the aforementioned GNU Affero General Public License v3.0 to the source code.</li>
           </ol>
         </li>
         <li>Accounts
-          <ol>
+          <ol class="tc">
             <li>You must be at least 18 years or of legal age to register an account and play the game.</li>
             <li>When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.</li>
             <li>You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service.</li>
@@ -58,7 +58,7 @@ ol.tc li:before
           </ol>
         </li>
         <li>Game Rules
-          <ol>
+          <ol class="tc">
             <li>You are only allowed to have one dominion per round.</li>
             <li>Your dominion name or ruler name must not be offensive, abusive, or misleading.</li>
             <li>You may not use any tools, software, application, scripts, or otherwise to automate any activities in the game.</li>
@@ -71,14 +71,14 @@ ol.tc li:before
           </ol>
         </li>
         <li>Links To Other Web Sites
-          <ol>
+          <ol class="tc">
             <li>Our Service may contain links to third-party web sites or services that are not owned or controlled by {{ $website_name }}.</li>
             <li>{{ $website_name }} has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that {{ $website_name }} shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</li>
             <li>We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.</li>
           </ol>
         </li>
         <li>Termination
-          <ol>
+          <ol class="tc">
             <li>We may terminate or suspend, limit, and/or restrict access to our Service immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms.</li>
             <li>All provisions of the Terms which by their nature should survive termination shall survive termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and limitations of liability.</li>
             <li>We may terminate or suspend your account immediately, without prior notice or liability, for any reason whatsoever, including without limitation if you breach the Terms</li>
@@ -88,29 +88,29 @@ ol.tc li:before
           </ol>
         </li>
         <li>Governing Law
-          <ol>
+          <ol class="tc">
             <li>These Terms shall be governed and construed in accordance with the laws of {{ $company_jurisdiction }}, without regard to its conflict of law provisions.</li>
             <li>Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between us regarding our Service, and supersede and replace any prior agreements we might have between us regarding the Service.</li>
           </ol>
         </li>
         <li>Changes
-          <ol>
+          <ol class="tc">
             <li>We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material we will try to provide at least 30 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.</li>
             <li>By continuing to access or use our Service after those revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, please stop using the Service.</li>
           </ol>
         </li>
         <li>Privacy Policy
-          <ol>
+          <ol class="tc">
             <li>These Terms also include the {{ $website_name }} Privacy Policy, available from this link: {{ route('legal.privacypolicy') }}</li>
           </ol>
         </li>
         <li>Contact Us
-          <ol>
+          <ol class="tc">
             <li>If you have any questions about these Terms, please contact us via email by {{ $contact_email }}.</li>
           </ol>
         </li>
         <li>General
-          <ol>
+          <ol class="tc">
             <li>If you do not agree with these Terms, you may not create an account.</li>
             <li>If you have already created an account and do not agree with the Terms, you must immediately cease taking part of {{ $website_url }}.</li>
             <li>Insofar as applicable, these Terms also apply to other chat rooms, discussion boards, and other websites directly associated with {{ $website_name }}.</li>
