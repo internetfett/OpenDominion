ccfefd2cdee7d79800a6b2d00416926f3d2213dd
Dr. Eki <dreki@lime.st>
2020-01-19 11:56:10 +0200

    Update techs.blade.php

--- app/resources/views/pages/dominion/techs.blade.php
+++ app/resources/views/pages/dominion/techs.blade.php
@@ -32,7 +32,7 @@
                                 </tr>
                             </thead>
                             @foreach ($techs as $tech)
-                            @if(count(array_diff($tech->prerequisites, $unlockedTechs)) == 0 or in_array($tech->key, $unlockedTechs))
+                            @if(count(array_diff($tech->prerequisites, $unlockedTechs)) == 0 or in_array($tech->key, $unlockedTechs) and $tech->enabled == 1)
                                 <tr class="{{ in_array($tech->key, $unlockedTechs) ? 'text-green' : 'text-default' }}">
                                     <td class="text-center">
                                         @if(in_array($tech->key, $unlockedTechs))
