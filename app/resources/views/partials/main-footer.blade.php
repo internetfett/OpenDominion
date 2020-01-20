115f1286939281cde19a43edb120db390237cacd
Dr. Eki <dreki@lime.st>
2020-01-20 21:07:43 +0200

    Update main-footer.blade.php

--- app/resources/views/partials/main-footer.blade.php
+++ app/resources/views/partials/main-footer.blade.php
@@ -20,4 +20,20 @@
     <i class="fa fa-file-text-o"></i> <a href="{{ route('legal.privacypolicy') }}">Privacy Policy</a> | <a href="{{ route('legal.termsandconditions') }}">Terms and Conditions</a>
     @endif
 
+    <a href="https://www.youtube.com/channel/UCGR9htOHUFzIfiPUsZapHhw" target="_blank" style="border:none; text-decoration:none;">
+    <img src="https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/youtube-icon_24x24.png" width="24" height="24" style="filter: gray; -webkit-filter: grayscale(1); filter: grayscale(1);" />
+    </a>
+
+    <a href="https://www.facebook.com/odarenagame/" target="_blank" style="border:none; text-decoration:none;">
+    <img src="https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/facebook-icon_24x24.png" width="24" height="24" style="filter: gray; -webkit-filter: grayscale(1); filter: grayscale(1);" />
+    </a>
+
+    <a href="https://twitter.com/OD_Arena" target="_blank" style="border:none; text-decoration:none;">
+    <img src="https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/twitter-icon_24x24.png" width="24" height="24" style="filter: gray; -webkit-filter: grayscale(1); filter: grayscale(1);" />
+    </a>
+
+    <a href="https://instagram.com/OD_Arena" target="_blank" style="border:none; text-decoration:none;">
+    <img src="https://68ef2f69c7787d4078ac-7864ae55ba174c40683f10ab811d9167.ssl.cf1.rackcdn.com/instagram-icon_24x24.png" width="24" height="24" style="filter: gray; -webkit-filter: grayscale(1); filter: grayscale(1);" />
+    </a>
+
 </footer>
