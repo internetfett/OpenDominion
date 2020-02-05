b036c5257f8f158915e0d87353aa21caf31f39ef
Dr. Eki <dreki@lime.st>
2020-02-05 13:25:50 +0200

    Update register.blade.php

--- app/resources/views/pages/round/register.blade.php
+++ app/resources/views/pages/round/register.blade.php
@@ -243,7 +243,7 @@
                       @if($round->hasStarted())
                       <p>This round has already started and ends {{ $round->end_date->format('l, jS \o\f F Y \a\t H:i') }}.</p>
                       <p>Protection lasts seven hours and starts immediately when you click the Register button.</p>
-                      <p>To help you get going, you will get 0.5% extra starting resources for every hour since the round started.</p>
+                      <p>To help you get going, you will get 1% extra starting resources for every hour since the round started (max +100%).</p>
 
                         @if ($discordInviteLink = config('app.discord_invite_link'))
                         <p>If you need any help or just want to chat, come join us on <a href="{{ $discordInviteLink }}" target="_blank">Discord</a>.</p>
@@ -252,7 +252,7 @@
                       @else
                       <p>The round starts on {{ $round->start_date->format('l, jS \o\f F Y \a\t H:i') }}.</p>
                       <p>Protection lasts seven hours.</p>
-                      <p>If 00:00 to 07:00 UTC are not convenient hours for you, consider registering a little later.</p>
+                      <p>If 12:00 to 19:00 UTC are not convenient hours for you, consider registering a little later. To compensate, you get 1% extra starting resources for every hour late you join (max +100%).</p>
 
                         @if ($discordInviteLink = config('app.discord_invite_link'))
                         <p>In the meantime, come join us on <a href="{{ $discordInviteLink }}" target="_blank">Discord</a>.</p>
