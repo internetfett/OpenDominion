fde8b98058e5a42cb7509d2180f75056f7c37743
Dr. Eki <dreki@lime.st>
2020-02-18 08:54:12 +0200

    Update CasualtiesCalculator.php

--- src/Calculators/Dominion/CasualtiesCalculator.php
+++ src/Calculators/Dominion/CasualtiesCalculator.php
@@ -245,7 +245,7 @@ class CasualtiesCalculator
      * @param int|null $slot Null is for non-racial units and thus used as draftees casualties multiplier
      * @return float
      */
-    public function getDefensiveCasualtiesMultiplierForUnitSlot(Dominion $dominion, Dominion $attacker, ?int $slot, array $units): float
+    public function getDefensiveCasualtiesMultiplierForUnitSlot(Dominion $dominion, Dominion $attacker, ?int $slot, array $units, float $landRatio): float
     {
         $multiplier = 1;
 
