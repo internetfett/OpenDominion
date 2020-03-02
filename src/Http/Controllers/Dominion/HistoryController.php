c713c07f8a6cad45791d562b3421b4d0f02eae60
Dr. Eki <dreki@lime.st>
2020-03-02 15:22:57 +0200

    Update HistoryController.php

--- src/Http/Controllers/Dominion/HistoryController.php
+++ src/Http/Controllers/Dominion/HistoryController.php
@@ -14,7 +14,7 @@ class HistoryController extends AbstractDominionController
 
         $history = DB::table('dominion_history')
                             ->where('dominion_history.dominion_id', '=', $selectedDominion->id)
-                            ->orderBy('dominion_history.created_at')
+                            ->orderBy('dominion_history.created_at', 'desc')
                             ->get();
 
         return view('pages.dominion.history', [
