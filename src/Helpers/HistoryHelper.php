d39a7463a9f18ab1b8e34b48859d0355767f2bc1
Dr. Eki <dreki@lime.st>
2020-03-02 15:46:25 +0200

    Update HistoryHelper.php

--- src/Helpers/HistoryHelper.php
+++ src/Helpers/HistoryHelper.php
@@ -10,21 +10,21 @@ class HistoryHelper
     public function getEventIcon(string $event): string
     {
         $icons = [
-            'tick' => 'ra ra-hourglass ra-fw',
-            'invade' => 'ra ra-crossed-swords ra-fw',
-            'construct' => 'fa fa-home fa-fw',
-            'destroy' => 'fa fa-home fa-fw',
-            'rezone' => 'fa fa-refresh fa-fw',
-            'change draft rate' => 'ra ra-sword ra-fw',
-            'daily bonus' => 'fa fa-plus fa-fw',
-            'tech' => 'fa fa-flask fa-fw',
-            'improve' => 'fa fa-arrow-up fa-fw',
-            'train' => 'ra ra-sword ra-fw',
-            'cast spell' => 'ra ra-fairy-wand ra-fw',
-            'release' => 'ra ra-sword ra-fw',
-            'perform espionage operation' => 'fa fa-user-secret fa-fw',
-            'bank' => 'fa fa-money fa-fw',
-            'explore' => 'ra ra-telescope ra-fw',
+          'bank' => 'fa fa-money fa-fw',
+          'cast spell' => 'ra ra-fairy-wand ra-fw',
+          'change draft rate' => 'ra ra-sword ra-fw',
+          'construct' => 'fa fa-home fa-fw',
+          'daily bonus' => 'fa fa-plus fa-fw',
+          'destroy' => 'fa fa-home fa-fw',
+          'explore' => 'ra ra-telescope ra-fw',
+          'improve' => 'fa fa-arrow-up fa-fw',
+          'invade' => 'ra ra-crossed-swords ra-fw',
+          'perform espionage operation' => 'fa fa-user-secret fa-fw',
+          'release' => 'ra ra-sword ra-fw',
+          'rezone' => 'fa fa-refresh fa-fw',
+          'tech' => 'fa fa-flask fa-fw',
+          'tick' => 'ra ra-hourglass ra-fw',
+          'train' => 'ra ra-sword ra-fw',
         ];
 
         return $icons[$event];
@@ -33,21 +33,21 @@ class HistoryHelper
     public function getEventName(string $event): string
     {
         $icons = [
-            'tick' => 'Tick',
-            'invade' => 'Invasion',
-            'construct' => 'Construction',
-            'destroy' => 'Destroy',
-            'rezone' => 'Rezoning',
-            'change draft rate' => 'Draft rate',
-            'daily bonus' => 'Daily bonus',
-            'tech' => 'Advancement',
-            'improve' => 'Improvement',
-            'train' => 'Training',
-            'cast spell' => 'Spell',
-            'release' => 'Release',
-            'perform espionage operation' => 'Espionage',
-            'bank' => 'Exchange',
-            'explore' => 'Explore',
+          'bank' => 'Exchange',
+          'cast spell' => 'Spell',
+          'change draft rate' => 'Draft rate',
+          'construct' => 'Construction',
+          'daily bonus' => 'Daily bonus',
+          'destroy' => 'Destroy',
+          'explore' => 'Explore',
+          'improve' => 'Improvement',
+          'invade' => 'Invasion',
+          'perform espionage operation' => 'Espionage',
+          'release' => 'Release',
+          'rezone' => 'Rezoning',
+          'tech' => 'Advancement',
+          'tick' => 'Tick',
+          'train' => 'Training',
         ];
 
         return $icons[$event];
