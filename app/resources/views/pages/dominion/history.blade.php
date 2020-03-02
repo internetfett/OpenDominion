ac2e23dc1e73344b0906a249b305e90357884668
Dr. Eki <dreki@lime.st>
2020-03-02 15:15:42 +0200

    Update history.blade.php

--- app/resources/views/pages/dominion/history.blade.php
+++ app/resources/views/pages/dominion/history.blade.php
@@ -3,56 +3,52 @@
 @section('page-header', 'History')
 
 @section('content')
-    <div class="row">
+<div class="row">
+  <div class="col-sm-12 col-md-9">
+      <div class="box box-primary">
+          <div class="box-header with-border">
+              <h3 class="box-title"><i class="fa fa-arrow-up fa-fw"></i> Improvements</h3>
+          </div>
 
-        <div class="col-sm-12 col-md-9">
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title"><i class="ra ra-book"></i> History</h3>
-                </div>
-                <div class="box-body no-padding">
-                    <div class="row">
-
-                      <div class="box-body table-responsive no-padding">
-                          <table class="table">
-                              <colgroup>
-                                  <col width="150">
-                                  <col width="150">
-                                  <col>
-                              </colgroup>
-                              <thead>
-                                  <tr>
-                                      <th>Date</th>
-                                      <th>Action</th>
-                                      <th>Details</th>
-                                  </tr>
-                              </thead>
-                              <tbody>
-                                  @foreach ($history as $event)
-                                      <tr>
-                                          <td>
-                                              {{ $event->created_at }}
-                                          </td>
-                                          <td>
-                                                <i class="{{ $historyHelper->getEventIcon($event->event) }}"></i> {{ $historyHelper->getEventName($event->event) }}</a>
-                                          </td>
-                                          <td>
-                                              <ul>
-                                              @foreach(json_decode($event->delta, TRUE) as $data => $delta)
-                                                <li>{{ $data }}: {{ $delta }}</li>
-                                              @endforeach
-                                              </ul>
-                                          </td>
-                                      </tr>
-                                  @endforeach
-                              </tbody>
-                          </table>
-                      </div>
-
+              <div class="box-body table-responsive no-padding">
+                  <table class="table">
+                      <colgroup>
+                          <col width="150">
+                          <col width="150">
+                          <col>
+                      </colgroup>
+                      <thead>
+                          <tr>
+                              <th>Date</th>
+                              <th>Action</th>
+                              <th>Details</th>
+                          </tr>
+                      </thead>
+                            <tbody>
+                                @foreach ($history as $event)
+                                    <tr>
+                                        <td>
+                                            {{ $event->created_at }}
+                                        </td>
+                                        <td>
+                                              <i class="{{ $historyHelper->getEventIcon($event->event) }}"></i> {{ $historyHelper->getEventName($event->event) }}</a>
+                                        </td>
+                                        <td>
+                                            <ul>
+                                            @foreach(json_decode($event->delta, TRUE) as $data => $delta)
+                                              <li>{{ $data }}: {{ $delta }}</li>
+                                            @endforeach
+                                            </ul>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
-                </div>
-            </div>
-        </div>
+
+                  </div>
+              </div>
+          </div>
 
         <div class="col-sm-12 col-md-3">
             <div class="box">
