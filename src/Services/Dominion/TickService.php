7f342972e542311bdf95337002520d0259a44c8e
Dr. Eki <dreki@lime.st>
2020-03-10 21:18:08 +0200

    Update TickService.php

--- src/Services/Dominion/TickService.php
+++ src/Services/Dominion/TickService.php
@@ -267,27 +267,22 @@ class TickService
                   }
                 }
 
-                // Improvements Decay: remove points from each improvement type.
-                if($dominion->tick->improvements_decay > 0)
-                {
-                  Log::info($dominion->id . ' has improvements decay.');
-                  foreach($this->improvementHelper->getImprovementTypes($dominion) as $improvementType)
-                  {
-                    dd($improvementType);
-                    $dominion->{'improvement_' . $improvementType} -= $dominion->{'improvement_' . $improvementType} * ($dominion->tick->improvements_decay / 10000);
-                  }
-                }
-                else
-                {
-                    Log::info($dominion->id . ' does not have improvements decay.');
-                }
-
                 DB::transaction(function () use ($dominion) {
                     if (!empty($dominion->tick->starvation_casualties)) {
                         $this->notificationService->queueNotification(
                             'starvation_occurred',
                             $dominion->tick->starvation_casualties
                         );
+                    }
+
+                    // Improvements Decay: remove points from each improvement type.
+                    if($dominion->tick->improvements_decay > 0)
+                    {
+                      Log::info($dominion->id . ' has improvements decay.');
+                      foreach($this->improvementHelper->getImprovementTypes($dominion) as $improvementType)
+                      {
+                        $dominion->{'improvement_' . $improvementType} -= $dominion->{'improvement_' . $improvementType} * ($dominion->tick->improvements_decay / 10000);
+                      }
                     }
 
                     $this->cleanupActiveSpells($dominion);
