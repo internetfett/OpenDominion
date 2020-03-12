a7fc9803cd7e4ade785c17b6304d1dd599d84d43
Dr. Eki <dreki@lime.st>
2020-03-12 21:07:56 +0200

    Update invade.blade.php

--- app/resources/views/pages/dominion/invade.blade.php
+++ app/resources/views/pages/dominion/invade.blade.php
@@ -10,35 +10,7 @@
     <div class="row">
 
         <div class="col-sm-12 col-md-9">
-            @if ($protectionService->isUnderProtection($selectedDominion))
-                <div class="box box-primary">
-                    <div class="box-header with-border">
-                        <h3 class="box-title"><i class="ra ra-crossed-swords"></i> Invade</h3>
-                    </div>
-                    <div class="box-body">
-                        You are currently under protection for <b>{{ $selectedDominion->protection_ticks }}</b> {{ str_plural('tick', $selectedDominion->protection_ticks) }} and may not invade during that time.
-                    </div>
-                </div>
-          @elseif (!$selectedDominion->round->hasStarted())
-              <div class="box box-primary">
-                  <div class="box-header with-border">
-                      <h3 class="box-title"><i class="ra ra-crossed-swords"></i> Invade</h3>
-                  </div>
-                  <div class="box-body">
-                      You cannot invade until the round has started.
-                  </div>
-              </div>
-            @elseif ($selectedDominion->morale < 50)
-                <div class="box box-primary">
-                    <div class="box-header with-border">
-                        <h3 class="box-title"><i class="ra ra-crossed-swords"></i> Invade</h3>
-                    </div>
-                    <div class="box-body">
-                        Your military needs at least 50% morale to invade others. Your military currently has {{ $selectedDominion->morale }}% morale.
-                    </div>
-                </div>
-            @else
-                <form action="{{ route('dominion.invade') }}" method="post" role="form" id="invade_form">
+            <form action="{{ route('dominion.invade') }}" method="post" role="form" id="invade_form">
                     @csrf
 
                     <div class="box box-primary">
@@ -435,7 +407,6 @@
                     </div>
 
                 </form>
-            @endif
         </div>
 
         <div class="col-sm-12 col-md-3">
